4721247bf0a5f1797f0457e16d176418d744ff7a
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-04-13 10:28:56 +0200

    Mise en place TodoList

---
+++ TodoList/webpack.config.js
@@ -0,0 +1,28 @@
+/* eslint-env node  */
+module.exports = {
+  devtool: 'source-map',
+  entry: './js/main.js',
+  output: {
+    filename: './dist/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                targets: {
+                  browsers: ['last 2 versions', 'ie >= 8']
+                }
+              }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
