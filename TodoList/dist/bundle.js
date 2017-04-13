bc832f4a577e06b2f67be0d4b3fc9b5da7103c3a
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-04-13 11:53:21 +0200

    Enoncé DOM

--- TodoList/dist/bundle.js
+++ TodoList/dist/bundle.js
@@ -73,7 +73,36 @@
 "use strict";
 
 
-console.log('Coucou');
+var formAddElt = document.querySelector('.todolist-add-form');
+var listElt = document.querySelector('.todolist-list');
+
+formAddElt.addEventListener('submit', function submitCb(e) {
+  e.preventDefault();
+  // e.currentTarget; // l'élément qui a déclenché cet Event (ici c'est le form)
+  var saisi = formAddElt.todo.value;
+
+  var divElt = document.createElement('div');
+  //divElt.className = 'todolist-row';
+  divElt.classList.add('todolist-row'); // PAS IE8
+  divElt.innerHTML = '<b>' + saisi + '</b>';
+
+  if (listElt.children.length) {
+    listElt.insertBefore(divElt, listElt.firstElementChild);
+  } else {
+    listElt.appendChild(divElt);
+  }
+});
+
+// 1 - Ajouter un bouton - sur chaque ligne
+// * écouter l'événement click
+// * Element.prototype.removeChild()
+// * Element.parentNode
+
+// 2 - Ajouter une checkbox sur chaque ligne et une générale (dans le form)
+// * <input type="checkbox">
+// * Au click de la checkbox générale tout cocher/décocher
+// * Element.prototype.querySelectorAll() (retourne un NodeList (pas un Array))
+// * HTMLInputElement.checked = true (ou false pour décocher)
 
 /***/ })
 /******/ ]);
--- TodoList/js/main.js
+++ TodoList/js/main.js
@@ -1,1 +1,33 @@
-console.log('Coucou');
+'use strict';
+
+const formAddElt = document.querySelector('.todolist-add-form');
+const listElt = document.querySelector('.todolist-list');
+
+formAddElt.addEventListener('submit', function submitCb(e) {
+  e.preventDefault();
+  // e.currentTarget; // l'élément qui a déclenché cet Event (ici c'est le form)
+  const saisi = formAddElt.todo.value;
+
+  const divElt = document.createElement('div');
+  //divElt.className = 'todolist-row';
+  divElt.classList.add('todolist-row'); // PAS IE8
+  divElt.innerHTML = `<b>${saisi}</b>`;
+
+  if (listElt.children.length) {
+    listElt.insertBefore(divElt, listElt.firstElementChild);
+  }
+  else {
+    listElt.appendChild(divElt);
+  }
+});
+
+// 1 - Ajouter un bouton - sur chaque ligne
+// * écouter l'événement click
+// * Element.prototype.removeChild()
+// * Element.parentNode
+
+// 2 - Ajouter une checkbox sur chaque ligne et une générale (dans le form)
+// * <input type="checkbox">
+// * Au click de la checkbox générale tout cocher/décocher
+// * Element.prototype.querySelectorAll() (retourne un NodeList (pas un Array))
+// * HTMLInputElement.checked = true (ou false pour décocher)
